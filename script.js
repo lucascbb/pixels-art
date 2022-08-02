56806e3bff224110d22bebec0b878bd6670b3841
lucascbb <lucasbaroquello@hotmail.com>
2022-08-02 19:29:36 -0300

    exercicio 6 e 5(bug)

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById('title').innerHTML = 'Paleta de Cores';
+
+document.getElementsByClassName("color")[0].style.backgroundColor = 'black';
+document.getElementsByClassName("color")[1].style.backgroundColor = 'darkblue';
+document.getElementsByClassName("color")[2].style.backgroundColor = 'orange';
+document.getElementsByClassName("color")[3].style.backgroundColor = 'darkgreen';
+
+document.getElementById('pixel-board').style.backgroundColor = "red";
+document.getElementById('pixel-board').style.width= "210px";
+document.getElementById('pixel-board').style.height= "210px";
+
+// document.getElementsByClassName("pixel")[5].style.marginLeft = '42px';
+// document.getElementsByClassName("pixel")[5].style.marginTop = '-210px';
+
+// document.getElementsByClassName("pixel")[6].style.marginLeft = '42px';
+// document.getElementsByClassName("pixel")[7].style.marginLeft = '42px';
+// document.getElementsByClassName("pixel")[8].style.marginLeft = '42px';
+// document.getElementsByClassName("pixel")[10].style.marginLeft = '42px';
+// document.getElementsByClassName("pixel")[10].style.marginTop = '-42px';
+// document.getElementsByClassName("pixel")[11].style.marginLeft = '0px';
+// document.getElementsByClassName("pixel")[11].style.marginTop = '-210px';
+// document.getElementsByClassName("pixel")[12].style.marginLeft = '0px';
+// document.getElementsByClassName("pixel")[13].style.marginLeft = '0px';
+// document.getElementsByClassName("pixel")[14].style.marginLeft = '0px';
+// document.getElementsByClassName("pixel")[15].style.marginLeft = '0px';
+
+// let teste = document.querySelector
