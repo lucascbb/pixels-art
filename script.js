a96cd85e93db096e34712bf027704841c12c278a
lucascbb <lucasbaroquello@hotmail.com>
2022-08-03 17:39:58 -0300

    exer 5

--- script.js
+++ script.js
@@ -9,19 +9,52 @@ document.getElementById('pixel-board').style.backgroundColor = "red";
 document.getElementById('pixel-board').style.width= "210px";
 document.getElementById('pixel-board').style.height= "210px";
 
-// document.getElementsByClassName("pixel")[5].style.marginLeft = '42px';
-// document.getElementsByClassName("pixel")[5].style.marginTop = '-210px';
 
-// document.getElementsByClassName("pixel")[6].style.marginLeft = '42px';
-// document.getElementsByClassName("pixel")[7].style.marginLeft = '42px';
-// document.getElementsByClassName("pixel")[8].style.marginLeft = '42px';
-// document.getElementsByClassName("pixel")[10].style.marginLeft = '42px';
-// document.getElementsByClassName("pixel")[10].style.marginTop = '-42px';
-// document.getElementsByClassName("pixel")[11].style.marginLeft = '0px';
-// document.getElementsByClassName("pixel")[11].style.marginTop = '-210px';
-// document.getElementsByClassName("pixel")[12].style.marginLeft = '0px';
-// document.getElementsByClassName("pixel")[13].style.marginLeft = '0px';
-// document.getElementsByClassName("pixel")[14].style.marginLeft = '0px';
-// document.getElementsByClassName("pixel")[15].style.marginLeft = '0px';
+// document.getElementsByClassName("color")[1].addEventListener("click", myFunction);
+// function myFunction() {
+//   document.getElementsByClassName("color")[1].style.backgroundColor = "black";
+// }
 
-// let teste = document.querySelector
+
+
+
+
+for(let index = 0; index < 25; index += 1){
+document.getElementsByClassName("pixel")[index].addEventListener("mousemove", myFunction);
+
+function myFunction(){
+    document.getElementsByClassName("pixel")[index].style.backgroundColor = "black";
+}
+}
+
+// document.getElementById("color-palette").onclick = jsCarregado
+
+// function jsCarregado(){
+//    if (document.getElementById("title").style.color = "white") {
+//     document.getElementById("title").style.color = "red"
+//    }
+// }
+
+
+// document.getElementsByClassName("pixel")[0].onclick = trocaCor
+
+// function trocaCor(){
+//     if (document.getElementsByClassName("pixel")[0].style.backgroundColor = "black") {
+//     document.getElementsByClassName("pixel")[0].style.backgroundColor = "white";}
+// }
+
+// for(let index = 0; index < 25; index += 1){
+// document.getElementsByClassName("reset-button").onclick = resetButton
+
+// function resetButton(){
+//     document.getElementsByClassName("pixel")[index].style.backgroundColor = "white";
+// }
+// }
+
+document.getElementsByClassName("reset-button").onclick = resetButton;
+
+function resetButton() {
+    for(let index = 0; index < 25; index += 1){
+    document.getElementsByClassName("pixel")[index].style.backgroundColor = "white";
+}
+}
