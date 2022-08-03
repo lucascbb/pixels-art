9ad44d3783bbfb7b2e1495eb644a6e8a75b7ef2b
lucascbb <lucasbaroquello@hotmail.com>
2022-08-03 18:46:46 -0300

    exer 8

--- script.js
+++ script.js
@@ -10,46 +10,62 @@ document.getElementById('pixel-board').style.width= "210px";
 document.getElementById('pixel-board').style.height= "210px";
 
 
-// document.getElementsByClassName("color")[1].addEventListener("click", myFunction);
-// function myFunction() {
-//   document.getElementsByClassName("color")[1].style.backgroundColor = "black";
-// }
+let trocaCor = [];
+
+window.onload = preto
 
 
+document.getElementsByClassName("color")[0].onclick = preto;
 
+function preto(){
+    selected =  "black"
+    }
 
+document.getElementsByClassName("color")[1].onclick = azul;
+
+function azul(){
+    selected =  "darkblue"
+    }
+
+document.getElementsByClassName("color")[2].onclick = laranja;
+    
+function laranja(){
+    selected =  "orange"
+    }
+
+document.getElementsByClassName("color")[3].onclick = verde;
+    
+function verde(){
+    selected =  "darkgreen"
+        }
+    
 
 for(let index = 0; index < 25; index += 1){
+    let corSelect = [];
 document.getElementsByClassName("pixel")[index].addEventListener("mousemove", myFunction);
 
 function myFunction(){
-    document.getElementsByClassName("pixel")[index].style.backgroundColor = "black";
+    document.getElementsByClassName("pixel")[index].style.backgroundColor = selected;
 }
 }
 
-// document.getElementById("color-palette").onclick = jsCarregado
-
-// function jsCarregado(){
-//    if (document.getElementById("title").style.color = "white") {
-//     document.getElementById("title").style.color = "red"
-//    }
-// }
 
 
-// document.getElementsByClassName("pixel")[0].onclick = trocaCor
 
-// function trocaCor(){
-//     if (document.getElementsByClassName("pixel")[0].style.backgroundColor = "black") {
-//     document.getElementsByClassName("pixel")[0].style.backgroundColor = "white";}
-// }
 
-// for(let index = 0; index < 25; index += 1){
-// document.getElementsByClassName("reset-button").onclick = resetButton
 
-// function resetButton(){
-//     document.getElementsByClassName("pixel")[index].style.backgroundColor = "white";
-// }
-// }
+
+
+
+
+
+
+
+
+
+
+
+
 
 document.getElementsByClassName("reset-button").onclick = resetButton;
 
